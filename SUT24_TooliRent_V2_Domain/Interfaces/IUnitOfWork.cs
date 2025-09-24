5369b599ad17bd5b03ee49fc7c96af985fbd6f52
tantting <jennyann.hayward82@gmail.com>
2025-09-24 10:43:31 +0200

    Added UnitOfWork.cs and its interface, included creaded a more or less empty BookingRepository.cs to work on later

---
+++ SUT24_TooliRent_V2_Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+namespace SUT24_TooliRent_V2_Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    IBookingRepository Bookings { get; }
+    IToolRepository Tools { get; }
+   Task <int> SaveChangesAsync();   
+   
+   
+}
