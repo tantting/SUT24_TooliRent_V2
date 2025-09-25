4a5481a8e5b4a28c56f86925b0db03335426a4b9
tantting <jennyann.hayward82@gmail.com>
2025-09-25 14:53:17 +0200

    Added CreateToolDto.cs

--- SUT24_TooliRent_V2_Application/DTOs/CreateToolDto.cs
+++ SUT24_TooliRent_V2_Application/DTOs/CreateToolDto.cs
@@ -1,3 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using SUT24_TooliRent_V2_Domain.Enums;
+
 namespace SUT24_TooliRent_V2_Application.DTOs;
 
-public record CreateToolDto();
+public record CreateToolDto
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    [Required]
+    public int WorkshopId { get; set; }
+
+    [Required]
+    public ToolCategory Category { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+    public bool DemandsCertification { get; set; }
+    public ToolCondition Condition { get; set; } = ToolCondition.New;
+}
