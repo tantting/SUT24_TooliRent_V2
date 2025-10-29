b30db56da26644da2d34f74b9d83925f9bf788c5
tantting <jennyann.hayward82@gmail.com>
2025-10-29 16:29:29 +0100

    Add SwaggerDefaultValues class to handle security requirements in Swagger documentation

---
+++ SUT24_TooliRent_V2_Swagger/SwaggerDefaultValues.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+namespace SUT24_TooliRent_V2_Swagger;
+
+public class SwaggerDefaultValues : IOpertionsFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var hasAuthorize = context.MethodInfo.DeclaringType!.GetCustomAttributes(true)
+                               .OfType<Microsoft.AspNetCore.Authorization.AuthorizeAttribute>().Any() ||
+                           context.MethodInfo.GetCustomAttributes(true)
+                               .OfType<Microsoft.AspNetCore.Authorization.AuthorizeAttribute>().Any();
+
+        if (hasAuthorize)
+        {
+            operation.Security ??= new List<OpenApiSecurityRequirement>();
+
+            operation.Security.Add(new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                }] = new string[] { }
+            });
+        }
+    }
+}
