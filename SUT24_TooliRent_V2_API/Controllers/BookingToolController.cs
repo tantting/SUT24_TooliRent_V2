61659f5368488364bcddf978288fb7c5ed080d62
Jenny-Ann Hayward <jenny-ann.hayward@seerefine.se>
2026-05-25 07:54:29 +0200

    Added authorization to controller

--- SUT24_TooliRent_V2_API/Controllers/BookingToolController.cs
+++ SUT24_TooliRent_V2_API/Controllers/BookingToolController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SUT24_TooliRent_V2_Application.DTOs.ToolDTOs;
@@ -7,6 +8,7 @@ namespace SUT24_TooliRent_V2.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BookingToolController : ControllerBase
     {
         private readonly IBookingToolService _bookingToolService;
