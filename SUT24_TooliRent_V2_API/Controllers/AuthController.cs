fd02cde767ea009342c244c6c3d1dcbc81f8a126
Jenny-Ann Hayward <jenny-ann.hayward@seerefine.se>
2026-05-26 15:48:33 +0200

    changed jwregisterClaim Name to Id instead of email

--- SUT24_TooliRent_V2_API/Controllers/AuthController.cs
+++ SUT24_TooliRent_V2_API/Controllers/AuthController.cs
@@ -135,7 +135,7 @@ namespace SUT24_TooliRent_V2.Controllers
 
             var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.Sub, user.Email!),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim(ClaimTypes.NameIdentifier, user.Id)
             };
