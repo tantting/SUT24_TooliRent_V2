fe3cddd4439dd383e0ff63c6332176fef57313a5
Jenny-Ann Hayward <jenny-ann.hayward@seerefine.se>
2026-05-27 10:03:22 +0200

    Made name nullable

--- SUT24_TooliRent_V2_Application/DTOs/ToolDTOs/UpdateToolDto.cs
+++ SUT24_TooliRent_V2_Application/DTOs/ToolDTOs/UpdateToolDto.cs
@@ -4,7 +4,7 @@ namespace SUT24_TooliRent_V2_Application.DTOs.ToolDTOs;
 
 public record UpdateToolDto
 {
-    public string? Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
     public string? Description { get; set; }
     public int? WorkshopId { get; set; }
     public bool? IsAvailable { get; set; } = true;
